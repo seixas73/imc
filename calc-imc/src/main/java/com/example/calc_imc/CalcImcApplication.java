107bff656bfa3558d9b9218fdd08a3861d9ead61
Artur Seixas <oseixas73@gmail.com>
2026-03-05 20:14:01 -0300

    Add files via upload

---
+++ calc-imc/src/main/java/com/example/calc_imc/CalcImcApplication.java
@@ -0,0 +1,13 @@
+package com.example.calc_imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcImcApplication.class, args);
+	}
+
+}
---
+++ calc-imc/src/test/java/com/example/calc_imc/CalcImcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calc_imc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcImcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
